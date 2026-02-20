3691af5794544b303813a4d0492abe8b6546df29
Vikash Rajput <rajputvi448@gmail.com>
2026-02-20 22:12:15 +0530

    Add files via upload

---
+++ Number.js
@@ -0,0 +1,21 @@
+//let a =10;
+//let b = 345.6821;
+// console.log(b.toFixed(2)); //decimal k bad kitni value
+// console.log(typeof b.toFixed(2)); //they give new value in string
+// console.log(b); //no change primitive(Number)
+// console.log(b.toPrecision(3));
+// console.log(b.toString());
+// console.log(b.toLocaleString());
+// console.log(b.valueOf());
+
+// let a = new Number(20);
+// let b = new Number (20);
+// console.log(a==b); // false 
+// let obj1 ={
+//     name: "Vikash"
+// }
+// let obj2 =obj1;
+// console.log(obj1==obj2); // true reference copy
+// console.log(typeof a, a);
+
+// primitive : copy by value
