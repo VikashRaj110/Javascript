699e955ccf4b0e8301811455ebb2389ba358a3fd
Vikash Rajput <rajputvi448@gmail.com>
2026-02-20 22:11:51 +0530

    Add files via upload

---
+++ conditional.js
@@ -0,0 +1,19 @@
+// let age =19;
+// if(age>=18){
+//     console.log("Eligible for voting");
+// }
+// else{
+//     console.log("Not Eligible for voting");
+// }
+
+// let age =89;
+// if(age<18){
+//     console.log("Eligible for voting");
+// }
+// else if(age>=60){
+//     console.log("Old");
+// }
+// else{
+//     console.log("Young");
+// }
+
