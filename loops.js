7d1f4dca606a3f48fad72b9608844ce2ff97eabb
Vikash Rajput <rajputvi448@gmail.com>
2026-02-23 01:41:42 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,20 @@
+// for loop
+// for(let i=0; i<=10;i++){
+//     console.log(i,"Love");
+// }
+
+//while loop
+
+// let i=0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+
+//do while
+// let i=0;
+// do{
+//    console.log(i) 
+//    i++;
+// }while (i<10)
+
